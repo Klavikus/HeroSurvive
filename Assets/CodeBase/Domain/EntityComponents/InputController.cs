4d3de6cad65ef532328a0ea44e01218cac18c8f9
Klavikus <912654q@gmail.com>
2023-03-04 10:50:47 +0300

    Add NewInputSystem

--- Assets/CodeBase/Domain/EntityComponents/InputController.cs
+++ Assets/CodeBase/Domain/EntityComponents/InputController.cs
@@ -1,4 +1,6 @@
+using System;
 using CodeBase.Domain.Data;
+using UnityEngine;
 
 namespace CodeBase.Domain.EntityComponents
 {
@@ -7,10 +9,28 @@ namespace CodeBase.Domain.EntityComponents
         private readonly string Horizontal = "Horizontal";
         private readonly string Vertical = "Vertical";
 
+        private PlayerInputActions _playerInputActions;
+
+        public void Initialize()
+        {
+            _playerInputActions = new PlayerInputActions();
+            _playerInputActions.Enable();
+        }
+
+        private void OnEnable()
+        {
+            _playerInputActions?.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _playerInputActions?.Disable();
+        }
+
         private void Update()
         {
-            float horizontal = SimpleInput.GetAxis(Horizontal);
-            float vertical = SimpleInput.GetAxis(Vertical);
+            float horizontal = _playerInputActions.Combat.Move.ReadValue<Vector2>().x;
+            float vertical = _playerInputActions.Combat.Move.ReadValue<Vector2>().y;
 
             InvokeInputUpdated(new InputData(horizontal, vertical));
         }
