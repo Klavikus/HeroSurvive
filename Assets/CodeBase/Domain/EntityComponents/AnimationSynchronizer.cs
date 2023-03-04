40910e9eca3190125bb33fa01d1e0424b6d4527b
Klavikus <912654q@gmail.com>
2023-03-04 10:49:17 +0300

    Set _stateHashes as static in AnimationSynchronizer.cs

--- Assets/CodeBase/Domain/EntityComponents/AnimationSynchronizer.cs
+++ Assets/CodeBase/Domain/EntityComponents/AnimationSynchronizer.cs
@@ -11,22 +11,19 @@ namespace CodeBase.Domain.EntityComponents
         private static readonly int Hitted = Animator.StringToHash("Hitted");
         private static readonly int Idle = Animator.StringToHash("Idle");
         private static readonly int Walk = Animator.StringToHash("Walk");
+        
+        private static readonly Dictionary<EntityState, int> _stateHashes = new()
+        {
+            {EntityState.Died, Died},
+            {EntityState.Hitted, Hitted},
+            {EntityState.Idle, Idle},
+            {EntityState.Walk, Walk},
+        };
 
         private readonly Animator _animator;
 
-        private readonly Dictionary<EntityState, int> _stateHashes;
-
-        public AnimationSynchronizer(Animator animator)
-        {
+        public AnimationSynchronizer(Animator animator) => 
             _animator = animator;
-            _stateHashes = new Dictionary<EntityState, int>
-            {
-                {EntityState.Died, Died},
-                {EntityState.Hitted, Hitted},
-                {EntityState.Idle, Idle},
-                {EntityState.Walk, Walk},
-            };
-        }
 
         public void ChangeState(EntityState newState)
         {
