62ce594c5e6e45b577d4887b99717b1023e1b3b5
Klavikus <912654q@gmail.com>
2023-03-04 08:52:50 +0300

    Optimized HeroModel.cs

--- Assets/CodeBase/MVVM/Models/HeroModel.cs
+++ Assets/CodeBase/MVVM/Models/HeroModel.cs
@@ -7,26 +7,21 @@ namespace CodeBase.MVVM.Models
 {
     public class HeroModel
     {
-        private List<AdditionalHeroProperty> _additionalProperties = new List<AdditionalHeroProperty>();
+        private readonly MainProperties _mainProperties = new MainProperties();
 
-        public HeroData CurrentSelectedHero;
         public event Action<HeroData> Changed;
+
+        public HeroData CurrentSelectedHero { get; private set; }
+        public MainProperties GetMainPropertiesData() => _mainProperties;
 
         public void SetHeroData(HeroData heroData)
         {
             CurrentSelectedHero = heroData;
-            _additionalProperties = heroData.AdditionalProperties;
+
+            foreach (AdditionalHeroProperty heroProperty in heroData.AdditionalProperties)
+                _mainProperties.UpdateProperty(heroProperty.BaseProperty, heroProperty.Value);
+
             Changed?.Invoke(heroData);
-        }
-
-        public MainProperties GetMainPropertiesData()
-        {
-            MainProperties result = new MainProperties();
-
-            foreach (AdditionalHeroProperty heroProperty in _additionalProperties)
-                result.UpdateProperty(heroProperty.BaseProperty, heroProperty.Value);
-
-            return result;
         }
     }
 }
