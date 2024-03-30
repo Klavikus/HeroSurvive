b975c241c3c33a40ea1b5c083ee99bc0531b6922
Klavikus <912654q@gmail.com>
2024-03-30 07:40:21 +0300

    Replace Leaderboard with MVP

---
+++ Assets/GameCore/Source/Controllers/Core/Presenters/LeaderBoardPresenter.cs
@@ -0,0 +1,15 @@
+﻿using Source.Controllers.Api;
+
+namespace GameCore.Source.Controllers.Core.Presenters
+{
+    public class LeaderBoardPresenter : IPresenter
+    {
+        public void Enable()
+        {
+        }
+
+        public void Disable()
+        {
+        }
+    }
+}
