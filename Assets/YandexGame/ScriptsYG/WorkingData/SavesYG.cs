378f4b87fe1ae1c0eaa81d96fa298b8f867ac43e
Klavikus <912654q@gmail.com>
2024-06-09 16:52:58 +0300

    Prepare for unity version upgrade

---
+++ Assets/YandexGame/ScriptsYG/WorkingData/SavesYG.cs
@@ -0,0 +1,27 @@
+﻿using static System.String;
+
+namespace YG
+{
+    [System.Serializable]
+    public class SavesYG
+    {
+        // "Технические сохранения" для работы плагина (Не удалять)
+        public int idSave;
+        public bool isFirstSession = true;
+        public string language = "ru";
+        public bool promptDone;
+
+        // Тестовые сохранения для демо сцены
+        // Можно удалить этот код, но тогда удалите и демо (папка Example)
+        public int money = 1;                       // Можно задать полям значения по умолчанию
+        public string newPlayerName = "Hello!";
+        public bool[] openLevels = new bool[3];
+        public string Data;
+
+        public SavesYG()
+        {
+            openLevels[1] = true;
+            Data = Empty;
+        }
+    }
+}
